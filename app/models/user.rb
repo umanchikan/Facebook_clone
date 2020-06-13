eb4bd7f854384f6bb0647a8277889de4777b488c
chiakisaito <a.cop.p2p2@gmail.com>
2020-06-13 17:11:46 +0900

    メールにバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :pictures
   validates :name, presence: true
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
 end
