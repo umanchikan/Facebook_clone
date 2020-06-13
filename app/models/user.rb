1908dc63a02afc3094c6770a5ee4411528259322
chiakisaito <a.cop.p2p2@gmail.com>
2020-06-13 17:15:09 +0900

    セキュアパスワードを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
