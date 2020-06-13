b05bbb8b1635fe9bfcca986ed1721d6985eab752
chiakisaito <a.cop.p2p2@gmail.com>
2020-06-13 17:10:22 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  has_many :pictures
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+end
