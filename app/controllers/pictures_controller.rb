4449b7e9c01e491e96cf750e40d4fecab79feef2
chiakisaito <a.cop.p2p2@gmail.com>
2020-06-20 06:56:57 +0900

    画像投稿後のフラッシュメッセージが2個表示されるエラーを修正

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -32,10 +32,8 @@ class PicturesController < ApplicationController
     respond_to do |format|
       if @picture.save
         format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
-        format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
       end
     end
   end
